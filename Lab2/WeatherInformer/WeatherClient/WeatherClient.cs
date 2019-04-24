47445e68cf95ed3add2f19b4d57c43c5e9be6397
Andriy Markus <andr98m@gmail.com>
2019-04-24 19:42:21 +0300

    web client for weather started to implement

--- Lab2/WeatherInformer/WeatherClient/WeatherClient.cs
+++ Lab2/WeatherInformer/WeatherClient/WeatherClient.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherInformer.WeatherClient.CurrentWeather;
 
 namespace WeatherInformer.WeatherClient
 {
     public class WeatherClient
     {
-        
+        private static readonly string APPID = @"ee7630a47a3c6a249628c0d2d09bb700";
+        private static readonly string baseurl = @"http://api.openweathermap.org/data/2.5";
+        public CurrentWeather.CurrentWeather GetCurrentWeather()
+        {
+            var url = baseurl +"/weather";
+            throw new NotImplementedException();
+        }
+
+        public Forecast5days3hours.ForecastBy3h GetForecast()
+        {
+            var url = baseurl + "/forecast";
+            throw new NotImplementedException();
+        }
+
+        private object Get(string url, Dictionary<string, string> queryParameters)
+        {
+            HttpClient client = new HttpClient();
+            throw new NotImplementedException();
+        }
     }
 }
+        
